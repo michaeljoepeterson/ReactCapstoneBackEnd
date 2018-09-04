071aef928cec5549d3e0253429d1e2c4d161c640
Michael Peterson <michaeljoepeterson@gmail.com>
2018-09-04 17:59:58 -0600

    hooked up back end with db

--- server.js
+++ server.js
@@ -3,15 +3,53 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require("mongoose");
 const app = express();
+const {User} = require("./models/user");
+const {PORT, DATABASE_URL } = require('./config');
+mongoose.Promise = global.Promise;
 app.use(morgan("common"));
 
-function runServer(){
-	app.listen(8080,() => {
-		console.log("listening on 8080");
-	});
+let server;
+
+function runServer(databaseUrl, port = PORT) {
+
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl,{ useNewUrlParser: true },err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        User.create({
+			username:"Test"
+		});
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
 }
 
 if (require.main === module) {
-  runServer();
+  runServer(DATABASE_URL).catch(err => console.error(err));
 }
+
+module.exports = { app, runServer, closeServer };
