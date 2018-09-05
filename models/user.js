7ccb560839e5882aa8e9f60b8cffb1b38bd002fc
Michael Peterson <michaeljoepeterson@gmail.com>
2018-09-05 17:23:08 -0600

    setup local and jwt passport strategies

--- models/user.js
+++ models/user.js
@@ -1,10 +1,26 @@
 'use strict';
 
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcryptjs');
+//todo think about and expand
 const userSchema = mongoose.Schema({
-	username: {type:String,required:true}
+	username: {type:String,required:true, unique:true}
+	password: {type:String,required:true}
 });
+
+userSchema.methods.serialize = function(){
+	return{
+		username: this.username || '',
+	}
+}
+
+userSchema.methods.validatePassword = function(password){
+	return bcrypt.compare(password, this.password);
+};
+
+userSchema.statics.hashPassword = function(password){
+	return bcrypt.hash(password,10);
+};
 
 const User = mongoose.model("User",userSchema);
 
