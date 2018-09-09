744c89164675906f5523b798b4442421d93206de
Michael Peterson <michaeljoepeterson@gmail.com>
2018-09-09 10:22:43 -0600

    modified user Schema

--- models/user.js
+++ models/user.js
@@ -3,9 +3,24 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 //todo think about and expand
+const heroSchema = mongoose.Schema({
+	health: {type:Number},
+	maxhealth: {type:Number},
+	abilityPoints: {type:Number},
+	maxAbilityPoints: {type:Number},
+	strength: {type:Number},
+	toughness: {type:Number},
+	agility: {type:Number},
+	superPowers: {type:Array}
+});
+
 const userSchema = mongoose.Schema({
 	username: {type:String,required:true, unique:true},
-	password: {type:String,required:true}
+	password: {type:String,required:true},
+	wins: {type:Number,default:0},
+	matches: {type:Number,default:0},
+	heroes: [heroSchema],
+	matchHistory: {type:Array}
 });
 
 userSchema.methods.serialize = function(){
